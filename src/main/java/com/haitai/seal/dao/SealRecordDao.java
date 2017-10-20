c9d1e735f0d66bcb00cc56fb046f54c2f1d453b7
zhaisongfang <songfang.zhai@haitaichina.com>
2017-10-20 10:34:49 +0800

    SeralApplyDao up1

--- src/main/java/com/haitai/seal/dao/SealRecordDao.java
+++ src/main/java/com/haitai/seal/dao/SealRecordDao.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-
+//SUN JENKINS2.83 sfz zsf
 import com.haitai.seal.bean.SealRecord;
 
 public interface SealRecordDao extends JpaRepository<SealRecord, String>,JpaSpecificationExecutor<SealRecord>{
